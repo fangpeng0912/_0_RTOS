d693c1fe06fc59ae68547bbac4442622011f73aa
fangpeng <fangpeng20060303@163.com>
2019-01-05 15:40:24 +0800

    first line format

--- _0_RTOS/1_tinyOS/main.c
+++ _0_RTOS/1_tinyOS/main.c
@@ -1,5 +1,5 @@
 //宏定义NVIC寄存器相关地址和内容
-#define NVIC_PENDSVPRI        0xE000ED22		 //PENDSV优先级寄存器，主要用来设置PENDSV优先级（可悬起系统调用，配置为最低优先级，注意与SVC区别：SVC为系统调用，不可悬起，且必须立即执行，如果无法立即执行，将上访成fault异常） 
+#define NVIC_PENDSVPRI        0xE000ED22	 //PENDSV优先级寄存器，主要用来设置PENDSV优先级（可悬起系统调用，配置为最低优先级，注意与SVC区别：SVC为系统调用，不可悬起，且必须立即执行，如果无法立即执行，将上访成fault异常） 
 #define NVIC_ICSR             0xE000ED04     //中断控制及状态寄存器，主要用来悬起PENSV异常（系统异常无需使能，只有外部中断需要使能）
 #define NVIC_PENDSV_PRISET    0x000000FF     //这样设置无论该内核用多少位表达优先级、优先级分组是什么样的，优先级都是最低的
 #define NVIC_PENDSVSET        0x10000000  
@@ -20,7 +20,7 @@ void delay(int count){
 
 //悬起PENDSV异常函数定义
 void trigglePendSV(void){
-	//设置优先级
+	//设置优先级  放大棚
 	MEM8(NVIC_PENDSVPRI)=NVIC_PENDSV_PRISET;
 	//悬起PENDSV中断
 	MEM32(NVIC_ICSR)=NVIC_PENDSVSET;
