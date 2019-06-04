c7b471fd0bd9eabd5b823be3550bf462ffb9a22b
fangpeng <fangpeng20060303@163.com>
2019-06-05 03:33:35 +0800

    delete some unnecessary file

--- _0_RTOS/2_tinyOS/Objects/switch.d
+++
@@ -1,9 +0,0 @@
-.\objects\switch.o: source\switch.c
-.\objects\switch.o: source\tinyos.h
-.\objects\switch.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\switch.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\Device\ARM\ARMCM3\Include\ARMCM3.h
-.\objects\switch.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm3.h
-.\objects\switch.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
-.\objects\switch.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
-.\objects\switch.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
-.\objects\switch.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\Device\ARM\ARMCM3\Include\system_ARMCM3.h
